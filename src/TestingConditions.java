dee9709958aa7b8e1e7ee40b358e81872ce9d292
Camila Lourenço <61992484+camilalourencos@users.noreply.github.com>
2020-11-04 08:31:55 -0300

    Conditions class

---
+++ src/TestingConditions.java
@@ -0,0 +1,19 @@
+
+public class TestingConditions {
+
+	public static void main(String[] args) {
+		//testing conditions		
+		int age = 32;
+		int tattoos = 0;
+		if(age<=29) {
+			System.out.println("You're not on the thirties yet! Phew!");
+		} else {
+			if(tattoos>=1) {
+				System.out.println("Well, at least you got tatts...");
+			} else {
+				System.out.println("Oh no...");
+			}
+		}
+	}
+}
+
