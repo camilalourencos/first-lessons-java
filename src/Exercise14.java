b8bc812badcfc28a5cb9b8190718912e23de69e1
Camila Lourenço <61992484+camilalourencos@users.noreply.github.com>
2020-11-04 17:40:25 -0300

    Adding exercise #14

---
+++ src/Exercise14.java
@@ -0,0 +1,14 @@
+
+public class Exercise14 {
+
+	public static void main(String[] args) {
+		
+		for(int number = 1; number <= 100; number++ )
+		if(number % 3 == 0) {
+			System.out.print(number);
+			System.out.print(" ");
+		}
+		
+	}
+
+}
