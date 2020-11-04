fc10eece7acab2da72d1f584169793235402f76b
Camila Lourenço <61992484+camilalourencos@users.noreply.github.com>
2020-11-04 12:27:54 -0300

    More examples with WHILE

--- src/TestingWhileCondition.java
+++ src/TestingWhileCondition.java
@@ -4,10 +4,17 @@ public class TestingWhileCondition {
 	public static void main(String[]args) {
 		
 		int counter = 0;
+		int total = 0;
 		
 		while(counter <= 10) {
-			System.out.println(counter);
-			counter = counter + 1;
+			
+			//total = total+counter;
+			total+= counter;
+			counter ++;
+			//counter = counter + 1;
+			//counter += 1; 
+			
+			System.out.println(total);
 		}
 		
 	}
