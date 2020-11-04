e151c984e06a8159c8f0ae605dbd8126e1af5266
Camila Lourenço <61992484+camilalourencos@users.noreply.github.com>
2020-11-04 10:04:05 -0300

    Boolean conditions

--- src/TestingConditions.java
+++ src/TestingConditions.java
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-
-public class TestingConditions2 {
-
-	public static void main(String[] args) {
-		//testing conditions with OR AND options		
-		int age = 32;
-		int tattoos = 0;
-		if(age<=29) {
-			System.out.println("You're not on the thirties yet! Phew!");
-		} else {
-			if(tattoos>=1) {
-				System.out.println("Well, at least you got tatts...");
-			} else {
-				System.out.println("Oh no...");
-			}
-		}
-	}
-}
-
-||||||| 3a2db7c
-=======
-
 public class TestingConditions {
 
 	public static void main(String[] args) {
@@ -39,4 +16,4 @@ public class TestingConditions {
 	}
 }
 
->>>>>>> 5e3402ded44bb14c46d971f5ed8d49815ae6686b
+
---
+++ src/TestingMoreConditions.java
@@ -0,0 +1,13 @@
+public class TestingMoreConditions {
+
+	public static void main(String[] args) {
+		//testing conditions		
+		int age = 30;
+		int tattoos = 0;
+		if(age<=29 || tattoos>=1) {
+			System.out.println("You're not THAT old");
+		} else {
+			System.out.println("Oh no...");
+			}
+	}
+}
